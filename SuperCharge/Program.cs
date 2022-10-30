9a8f5c64398cefc58cdf682306e184b8d992e37a
abhijith <abhijith@supergaming.com>
2022-10-30 18:43:09 +0530

    initial file commit

---
+++ SuperCharge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperCharge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
